15c54b7e5bb4a970133451158ef064886e4b9be1
sznitos <sznitos@gmail.com>
2024-05-26 11:02:18 +0200

    routing crash - fix

--- P7/ctrl.php
+++ P7/ctrl.php
@@ -1,4 +1,13 @@
-<?php require_once 'init.php';
+<?php
+require_once 'init.php';
+// Rozszerzenia:
+// Dodanie klasy Router oraz Route, które realizują idee przedstawione poprzednio, ale na wyższym poziomie i obiektowo.
+// Po pierwsze rezygnujemy ze struktury 'switch' w kontrolerze głównym i zastępujemy ją tablicą ścieżek przechowywaną
+// wewnątrz obiektu routera. Router powstaje w skrypcie init.php i jak inne ważne obekty jest dostępny przez getRouter().
+
+// Odpowiednio nazwane metody routera realizują wszystkie zadania iplementowane uprzednio w funkcji control oraz strukturze 'switch'.
+
+// Oczywiście tym samym znika funkcja 'control' - jest ona prywatną metodą routera.
 
 getRouter()->setDefaultRoute('calcShow'); // akcja/ścieżka domyślna
 getRouter()->setLoginRoute('login'); // akcja/ścieżka na potrzeby logowania (przekierowanie, gdy nie ma dostępu)
